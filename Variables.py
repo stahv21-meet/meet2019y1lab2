fea7e5aa372655d06612ffabe8863d328825c0ca
stahv21-meet <stahv21@meet.mit.edu>
2019-07-22 07:30:27 -0400

    Stopping here

---
+++ Variables.py
@@ -0,0 +1,37 @@
+Python 3.6.8 (default, Jan 14 2019, 11:02:34) 
+[GCC 8.0.1 20180414 (experimental) [trunk revision 259383]] on linux
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> a = 5
+>>> b = 10
+>>> a+b
+15
+>>> a = b
+>>> b = a
+>>> a + b
+20
+>>> a =5
+>>> c = a
+>>> a=b
+>>> b = c
+>>> print(b)
+5
+>>> print(a)
+10
+>>> four - '4'
+Traceback (most recent call last):
+  File "<pyshell#13>", line 1, in <module>
+    four - '4'
+NameError: name 'four' is not defined
+>>> four = '4'
+>>> print(four*3)
+444
+>>> five = 4
+>>> print(five*3)
+12
+
+>>> my_name = "student"
+>>> print(“Hi,“ + my_name)
+
+
+
+ 
---
+++ turtletry.py
@@ -0,0 +1,21 @@
+import turtle
+
+t = turtle.Turtle()
+#s
+
+t.penup()
+t.goto(100 -200, 200)
+t.pendown()
+
+t.goto(40 - 200, 200)
+t.goto(0 -200, 150)
+t.goto(0 -200, 100)
+t.goto(100 -200, 100)
+t.goto(100 -200, 50)
+t.goto(60 -200, 0)
+t.goto(0 -200, 0)
+
+t.penup()
+t.goto(175 -200,0)
+t.pendown
+
